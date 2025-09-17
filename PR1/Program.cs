7cde0e13efff9a0ccef78b0377193f90b88d1250
tuman <dtuman294@gmail.com>
2025-09-17 14:08:57 +0300

    Создание структуры для хранения данных

--- PR1/Program.cs
+++ PR1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    // Структура для хранения информации о трате
+    struct Expense
+    {
+        public string Name;      // Название товара/услуги
+        public decimal Amount;   // Сумма траты в рублях
 
+        // Конструктор для создания новой траты
+        public Expense(string name, decimal amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+    }
+}
