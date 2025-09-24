badaaf7014d6b0f3e13221a91c5f27c00a58953e
tuman <dtuman294@gmail.com>
2025-09-24 12:53:48 +0300

    основной метод управления программой

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -1,12 +1,61 @@
-﻿class TextStatistics
+﻿class TextAnalyzer
 {
-    public string Text { get; set; }               // Исходный текст (сокращенный)
-    public int WordCount { get; set; }            // Количество слов
-    public string ShortestWord { get; set; }      // Самое короткое слово
-    public int SentenceCount { get; set; }        // Количество предложений
-    public int VowelCount { get; set; }           // Количество гласных букв
-    public int ConsonantCount { get; set; }       // Количество согласных букв
-    public string LongestWord { get; set; }       // Самое длинное слово
-    public Dictionary<char, int> LetterFrequency { get; set; } // Частота каждой буквы
-    public DateTime AnalysisTime { get; set; }    // Время анализа
+    class TextStatistics
+    {
+        public string Text { get; set; }              // Исходный текст (сокращенный)
+        public int WordCount { get; set; }            // Количество слов
+        public string ShortestWord { get; set; }      // Самое короткое слово
+        public int SentenceCount { get; set; }        // Количество предложений
+        public int VowelCount { get; set; }           // Количество гласных букв
+        public int ConsonantCount { get; set; }       // Количество согласных букв
+        public string LongestWord { get; set; }       // Самое длинное слово
+        public Dictionary<char, int> LetterFrequency { get; set; } // Частота каждой буквы
+        public DateTime AnalysisTime { get; set; }    // Время анализа
+    }
+
+    private static List<TextStatistics> allStatistics = new List<TextStatistics>();
+
+    private static readonly HashSet<char> vowels = new HashSet<char>
+    {
+        'а', 'е', 'ё', 'и', 'о', 'и', 'у', 'ы', 'ю', 'э', 'я', 
+        'a', 'e', 'y', 'u', 'i', 'o'
+    };
+
+    private static readonly HashSet<char> consanats = new HashSet<char>
+    {
+        'й', 'ц', 'к', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ф', 'в', 'п', 'р', 'л', 'д', 'ж', 'ч', 'с', 'м', 'т', 'б',
+        'q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'
+    };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Анализатор текста ===");
+
+        //bool continueWorking = true;
+
+        while (true)
+        {
+            ShowMainMenu();
+            string choice = Console.ReadLine(); 
+
+            switch(choice)
+            {
+                case "1":
+                    AnalyzeNewText();
+                    break;
+                 case "2":
+                    ShowPreviosStatistics();
+                    break;
+                 case "3":
+                    //continueWorking = false;
+                    Console.WriteLine("До свидания");
+                    break;
+                 default:
+                    Console.WriteLine("Ошибка выбора");
+                    break;
+            }
+        }
+    }
+
+    
 }
