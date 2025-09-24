647c404a0988f8dbd5db4b7e01eafb52d4465068
tuman <dtuman294@gmail.com>
2025-09-24 11:24:37 +0300

    Хранение стастистики

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿class TextStatistics
+{
+    public string Text { get; set; }               // Исходный текст (сокращенный)
+    public int WordCount { get; set; }            // Количество слов
+    public string ShortestWord { get; set; }      // Самое короткое слово
+    public int SentenceCount { get; set; }        // Количество предложений
+    public int VowelCount { get; set; }           // Количество гласных букв
+    public int ConsonantCount { get; set; }       // Количество согласных букв
+    public string LongestWord { get; set; }       // Самое длинное слово
+    public Dictionary<char, int> LetterFrequency { get; set; } // Частота каждой буквы
+    public DateTime AnalysisTime { get; set; }    // Время анализа
+}
