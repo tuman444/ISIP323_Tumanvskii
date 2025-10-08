26ca486729529150da5d1b21e6728d5fdec75c59
tuman <dtuman294@gmail.com>
2025-10-08 13:37:44 +0300

    список жанров

--- PR4/Program.cs
+++ PR4/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace library
+{
+    public enum Genre // список жанров
+    { 
+        Fantasy,
+        ScienceFiction,
+        Mystery,
+        Romance,
+        Horror,
+        Biography
+    }
+}
