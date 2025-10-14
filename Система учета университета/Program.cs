3cfd2909de48bae28759ba7b4e5bcdb2eb3ed3ce
Dima <Dima@DESKTOP-320DAQ9>
2025-10-14 20:22:07 +0300

    Декомпозиция. Класс для всех людей в университете и его конструктор

--- Система учета университета/Program.cs
+++ Система учета университета/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace UniversityManagementSystem
+{
+    public abstract class Person //класс для всех людей в университете
+    {
+
+        public person() //конструктор класса
+        {
+
+        }
+
+    }
+}
