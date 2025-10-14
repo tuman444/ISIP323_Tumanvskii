dfc423b4a50a0c313db90dfef5fafd2199cc0408
Dima <Dima@DESKTOP-320DAQ9>
2025-10-14 20:26:35 +0300

    Декомпозиция . Абстрактный метод для полиморфизма Виртуальный метод

--- Система учета университета/Program.cs
+++ Система учета университета/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace UniversityManagementSystem
+﻿using System.Xml.Linq;
+
+namespace UniversityManagementSystem
 {
     public abstract class Person //класс для всех людей в университете
     {
@@ -6,6 +8,15 @@
         public person() //конструктор класса
         {
 
+        }
+
+        // Абстрактный метод для полиморфизма 
+        public abstract string GetInfo();
+
+        // Виртуальный метод 
+        public virtual string GetBasicInfo()
+        {
+            
         }
 
     }
