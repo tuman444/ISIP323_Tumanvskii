f567c8cb9995df0f98a6e42532c6d6eed0c4103c
tuman <dtuman294@gmail.com>
2025-10-18 15:09:17 +0300

    класс игрока метод атаки метод защиты метод получения урона метод лечения метод экипировки оружия метод экипировки доспех

--- PR6/Program.cs
+++ PR6/Program.cs
@@ -9,7 +9,7 @@
         }
         public abstract void Use(Player player);
     }
-    public class Weapon : Item //класс оружия
+    public class Weapon : Item //класс доспех
     {
         public int Damage {  get; private set; }
         public  Weapon(string name, int damage) : base (name)
@@ -51,10 +51,46 @@
     }
     public class Player // класс игрока
     {
+        public int HP { get; private set; }
+        public int MaxHP { get; private set; }
+        public Weapon CurrentWeapon { get; private set; }
+        public Armor CurrentArmor { get; private set; }
+        public bool IsFrozen { get; set; }
+        public Player(int maxHP)
+        {
+            MaxHP = maxHP;
+            HP = maxHP;
+            CurrentWeapon = new Weapon("Кулаки", 5);
+            CurrentArmor = new Armor("Одежда", 2);
+        }
+        public void Attack(Enemy enemy)
+        {
 
+        }
+        public void Defend()
+        {
+
+        }
+        public void TakeDamage(int damage)
+        {
+
+        }
+        public void Heal(int amount)
+        {
+
+        }
+        public void EquipWeapon(Weapon weapon)
+        {
+
+        }
+        public void EquipArmor(Armor armor)
+        {
+
+        }
     }
     public abstract class Enemy // класс врагов
     {
+        public string Name { get; protected set; }
 
     }
     public class Goblin : Enemy // класс врагов гоблин, скелет и маг с наледием от главного класса врагов
