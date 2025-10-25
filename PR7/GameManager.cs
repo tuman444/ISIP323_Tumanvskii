083a60aa0c355fb60b8eba5b52cee06eaee5e601
tuman <dtuman294@gmail.com>
2025-10-25 22:20:19 +0300

    Дикомп. Метод для первоначальной проверки и настройки игры Обновление отложенных поставок обработка одного хода Показ меню магазина для закупки деталей Проверка условия проигрыша

--- PR7/GameManager.cs
+++ PR7/GameManager.cs
@@ -9,5 +9,27 @@ using System.Data.Entity;
 
 namespace CarServiceGame
 {
-    
+    public static class GameManager 
+    {
+        public static void InitializeGame() //Метод для первоначальной проверки и настройки игры
+        {
+
+        }
+        public static void UpdateDeliveries() // Обновление отложенных поставок
+        {
+
+        }
+        public static void ProcessClientTurn() // обработка одного хода
+        {
+
+        }
+        public static void ShowShop() // Показ меню магазина для закупки деталей
+        {
+
+        }
+        public static bool CheckGameOver() // Проверка условия проигрыша
+        {
+
+        }
+    }
 }
