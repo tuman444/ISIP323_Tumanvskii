c355c892f66fda34fb1ea44dcbcc62d899a5a1d8
tuman <dtuman294@gmail.com>
2025-10-28 10:35:17 +0300

    главный метод main для входа в игру

--- PR7/Program.cs
+++ PR7/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarServiceGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,44 @@ namespace CarServiceDB
     {
         static void Main(string[] args)
         {
+            try
+            {
+                GameManager.InitializeGame();
 
+                // Основной игровой цикл
+                while (true)
+                {
+                    // 1. Сначала обновляем доставки (задание "спустя 2 машины")
+                    GameManager.UpdateDeliveries();
+
+                    // 2. Обрабатываем нового клиента
+                    GameManager.ProcessClientTurn();
+
+                    // 3. Проверяем, не проиграли ли мы
+                    if (GameManager.CheckGameOver())
+                    {
+                        break; // Выход из цикла, если игра окончена
+                    }
+
+                    // 4. Предлагаем зайти в магазин
+                    GameManager.ShowShop();
+
+                    Console.WriteLine("\nНажмите Enter, чтобы принять следующего клиента...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Задание: "Программа не должна вылетать в процессе работы"
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("\n!!! КРИТИЧЕСКАЯ ОШИБКА !!!");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Программа будет завершена.");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Нажмите Enter для выхода...");
+            Console.ReadLine();
         }
     }
 }
