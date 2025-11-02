ab26ad1dc924cfb65253fcef247cdedbf1ad9139
tuman <dtuman294@gmail.com>
2025-11-02 16:47:18 +0300

    подключение бд

---
+++ PR8/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR8;
+
+namespace MarketPlace
+{
+    internal class Core  // подключение бд
+    {
+        public static MarketPlaceNagievEntities Context = new MarketPlaceNagievEntities();  
+    }
+}
