894b9f60ddb1cf5e35ce5bbefcae51a41f9e5afa
tuman <dtuman294@gmail.com>
2025-11-02 18:19:48 +0300

    Реализация методов для регистрации, входа

--- PR8/Program.cs
+++ PR8/Program.cs
@@ -10,18 +10,110 @@ namespace MarketPlace
 {
     internal class Program
     {
+        // переменная для хранения данных о вошедшем пользователе.
+        // null == "Гость"
+        private static Users currentUser = null;
+
         #region Методы Регистрации, Входа и Выхода
         private static void Register() // метод регистрации
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("== Регистрация нового пользователя ==");
 
+            Console.Write("Введите Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Введите Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string pass1 = Console.ReadLine();
+
+            Console.Write("Подтвердите пароль: ");
+            string pass2 = Console.ReadLine();
+            
+            if (pass1 == pass2) // проверка на совпадение паролей
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: Пароли не совпадают!");
+                Console.ReadLine();
+            }
+            if (Core.Context.Users.Any(u => u.Username == username || u.Email == email)) //Проверка, занят ли Email или Username
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: Пользователь с таким Email или Username уже существует!");
+                Console.ReadLine();
+            }
+            string passHash = SimpleHash(pass1); // хеширование пароля
+            
+            Users newUser = new Users // Добавление пользователя в "Users"
+            {
+                Username = username,
+                Email = email,
+                PasswordHash = passHash
+            };
+            Core.Context.Users.Add(newUser);
+
+            try // сохраняем пользователя
+            {
+                Core.Context.SaveChanges();
+
+                // 6d. Создание корзины для пользователя
+                Carts newCart = new Carts
+                {
+                    UserID = newUser.UserID // <-- UserID стал доступен после SaveChanges()
+                };
+                Core.Context.Carts.Add(newCart);
+                Core.Context.SaveChanges();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Регистрация прошла успешно! Теперь вы можете войти.");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка при сохранении в БД: {ex.Message}");
+            }
+
+            Console.ReadLine();
         }
         private static void Login() // метод входа в аккаунт
         {
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine("== Вход в аккаунт ==");
 
+            Console.Write("Введите Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            string passHash = SimpleHash(password); // Хэшируем введенный пароль
+
+            Users user = Core.Context.Users.FirstOrDefault(u => u.Email == email && u.PasswordHash == passHash); // Ищем пользователя в БД
+
+            if (user == null) // Если не найден
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: Неверный Email или пароль!");
+                Console.ReadLine();
+            }
+            else // Если найден - сохраняем его в currentUser
+            {
+                currentUser = user;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Добро пожаловать, {currentUser.Username}!");
+                Console.ReadLine();
+            }
         }
         private static void Logout() // метод выхода из аккаунта
         {
-
+            currentUser = null;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Вы вышли из аккаунта.");
+            Console.ReadLine();
         }
         #endregion
 
