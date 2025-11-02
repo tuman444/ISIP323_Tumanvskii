6408037bd503b64dae6193410b226242bf12c31b
tuman <dtuman294@gmail.com>
2025-11-02 17:26:59 +0300

    Дикомп Методы Регистрации, Входа и Выхода

--- PR8/Program.cs
+++ PR8/Program.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using PR8; 
 
-namespace PR8
+namespace MarketPlace
 {
     internal class Program
     {
+        #region Методы Регистрации, Входа и Выхода
+        private static void Register() // метод регистрации
+        {
+
+        }
+        private static void Login() // метод входа в аккаунт
+        {
+
+        }
+        private static void Logout() // метод выхода из аккаунта
+        {
+
+        }
+        #endregion
+
         static void Main(string[] args)
         {
         }
